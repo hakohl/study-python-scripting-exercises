e24dea4ca58357c00153bd60e6eb4e9132794272
Harald Kohlmann <Harald.Kohlmann@gmx.de>
2024-02-19 11:18:00 +0100

    changes till 01-01

--- 01-basic-python/01-arithmetic/03-cakes/student.py
+++ 01-basic-python/01-arithmetic/03-cakes/student.py
@@ -1,1 +1,8 @@
 # Write your code here
+
+def cakes(eggs, butter, flour):
+    maxByEggs = eggs // 5
+    maxByButter = butter // 250
+    maxByFlour = flour // 250
+
+    return min(maxByEggs, maxByButter, maxByFlour)
